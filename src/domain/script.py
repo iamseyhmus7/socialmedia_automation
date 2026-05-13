a3f492925aa337953e20570c45e3e72aef05e6ec
iamseyhmus7 <seyhmusok01@gmail.com>
2026-05-13 18:57:01 +0300

    src/domain/script.py: Script modelinin veri sözlüğünden oluşturulma (from_dict) mantığı, yeni alanlar ve yapılandırılmış veri desteği ile genişletildi.

--- src/domain/script.py
+++ src/domain/script.py
@@ -23,20 +23,24 @@ class Script:
 
     @classmethod
     def from_dict(cls, data: dict[str, Any]) -> "Script":
+        script = dict(data.get("script") or {})
+        voice_plan = dict(data.get("voice_plan") or {})
+        media_plan = dict(data.get("media_plan") or {})
+        music = dict(media_plan.get("music") or {})
         return cls(
-            hook=str(data.get("hook", "")),
-            body=str(data.get("body", "")),
-            outro=str(data.get("outro", "")),
-            shock_hook=str(data.get("shock_hook", data.get("hook", ""))),
-            tension_body=str(data.get("tension_body", data.get("body", ""))),
-            payoff_outro=str(data.get("payoff_outro", data.get("outro", ""))),
-            loop_ending=str(data.get("loop_ending", "")),
+            hook=str(script.get("hook") or data.get("hook", "")),
+            body=str(script.get("body") or data.get("body", "")),
+            outro=str(script.get("outro") or data.get("outro", "")),
+            shock_hook=str(script.get("shock_hook") or data.get("shock_hook", data.get("hook", ""))),
+            tension_body=str(script.get("tension_body") or data.get("tension_body", data.get("body", ""))),
+            payoff_outro=str(script.get("payoff_outro") or data.get("payoff_outro", data.get("outro", ""))),
+            loop_ending=str(script.get("loop_ending") or data.get("loop_ending", "")),
             style=str(data.get("style", "aggressive_viral_motivation")),
             hook_pexels_search_term=str(data.get("hook_pexels_arama_terimi", "")),
             pexels_theme=str(data.get("pexels_arama_temasi", "")),
             pexels_keywords=list(data.get("pexels_anahtar_kelimeleri") or []),
-            freesound_search_term=str(data.get("freesound_arama_terimi", "epic motivational")),
-            highlighted_words=list(data.get("vurgulanacak_kelimeler") or []),
+            freesound_search_term=str(music.get("search_query") or data.get("freesound_arama_terimi", "epic motivational")),
+            highlighted_words=list(voice_plan.get("highlighted_words") or data.get("vurgulanacak_kelimeler") or []),
             raw=dict(data),
         )
 
